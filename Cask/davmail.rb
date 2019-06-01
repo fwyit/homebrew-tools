34845214f59163904141773a1baddfbd81db1b1d
Jam <jianliu@newbanker.cn>
2019-06-01 16:17:39 +0800

    syn 同步当前应用版本 @2019060116

--- Cask/davmail.rb
+++ Cask/davmail.rb
@@ -1,47 +1,16 @@
-class Davmail < Formula
-  desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
-  homepage "https://davmail.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/davmail/davmail/5.2.0/davmail-5.2.0-2961.zip"
-  version "5.2.0"
-  sha256 "a50ae84879b49502b82e8387deef2ddcc222a564e6670d9642ef9f799ed4864b"
+cask 'davmail' do
+  version '4.9.0-2652'
+  sha256 '463591fefd8ae9af7fd1e7f78f6ef278a61b5dbd3c72bff22f58ad1996975b13'
 
-  bottle :unneeded
+  # downloads.sourceforge.net/davmail was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/davmail/DavMail-MacOSX-#{version}.app.zip"
+  appcast 'https://sourceforge.net/projects/davmail/rss'
+  name 'DavMail'
+  homepage 'https://davmail.sourceforge.io/'
 
-  def install
-    libexec.install Dir["*"]
-    bin.write_jar_script libexec/"davmail.jar", "davmail", "-Djava.awt.headless=true"
-  end
+  app 'DavMail.app'
 
-  plist_options :manual => "davmail"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin</string>
-        </dict>
-        <key>KeepAlive</key>
-        <false/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/davmail</string>
-        </array>
-        <key>StartInterval</key>
-        <integer>300</integer>
-        <key>RunAtLoad</key>
-        <true />
-        <key>StandardErrorPath</key>
-        <string>/dev/null</string>
-        <key>StandardOutPath</key>
-        <string>/dev/null</string>
-      </dict>
-    </plist>
-  EOS
+  caveats do
+    depends_on_java
   end
 end
--- Cask/wireshark.rb
+++ Cask/wireshark.rb
@@ -1,81 +1,38 @@
-class Wireshark < Formula
-  desc "Graphical network analyzer and capture tool"
-  homepage "https://www.wireshark.org"
-  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.0.1.tar.xz"
-  sha256 "86864c3d0f6c2311992a98d8ea7dfd429097fe62dae2e5516e1a2f6bef2ac08c"
-  head "https://code.wireshark.org/review/wireshark", :using => :git
+cask 'wireshark' do
+  version '3.0.2'
+  sha256 '2e32ed900bc0bb85430ebe45eef39c5097423eb8cb3a7bd4b08e704f599ab430'
 
-  bottle do
-    sha256 "7f51031f7e0602485abed86b128c3b922e10dd663708764c44ef2ff807d8df4d" => :mojave
-    sha256 "a125993f0413171079a00ce17bb41125ab843a223feb2c5b66df4f78ec9229d2" => :high_sierra
-    sha256 "6f12831257770c9d36b042df85f8ba5ed553296f1f36e918cf3097fbec165ea6" => :sierra
+  url "https://www.wireshark.org/download/osx/Wireshark%20#{version}%20Intel%2064.dmg"
+  appcast 'https://www.wireshark.org/download/osx/'
+  name 'Wireshark'
+  homepage 'https://www.wireshark.org/'
+
+  conflicts_with cask: 'wireshark-chmodbpf'
+  depends_on macos: '>= :sierra'
+
+  pkg "Wireshark #{version} Intel 64.pkg"
+
+  uninstall_preflight do
+    set_ownership '/Library/Application Support/Wireshark'
+    system_command '/usr/sbin/dseditgroup', args: ['-o', 'delete', 'access_bpf'], sudo: true
   end
 
-  depends_on "cmake" => :build
-  depends_on "c-ares"
-  depends_on "glib"
-  depends_on "gnutls"
-  depends_on "libgcrypt"
-  depends_on "libmaxminddb"
-  depends_on "libsmi"
-  depends_on "libssh"
-  depends_on "lua@5.1"
-  depends_on "nghttp2"
+  uninstall pkgutil:   'org.wireshark.*',
+            launchctl: 'org.wireshark.ChmodBPF',
+            delete:    [
+                         '/private/etc/manpaths.d/Wireshark',
+                         '/private/etc/paths.d/Wireshark',
+                         '/usr/local/bin/capinfos',
+                         '/usr/local/bin/dftest',
+                         '/usr/local/bin/dumpcap',
+                         '/usr/local/bin/editcap',
+                         '/usr/local/bin/mergecap',
+                         '/usr/local/bin/randpkt',
+                         '/usr/local/bin/rawshark',
+                         '/usr/local/bin/text2pcap',
+                         '/usr/local/bin/tshark',
+                         '/usr/local/bin/wireshark',
+                       ]
 
-  def install
-    args = std_cmake_args + %W[
-      -DENABLE_CARES=ON
-      -DENABLE_GNUTLS=ON
-      -DENABLE_MAXMINDDB=ON
-      -DBUILD_wireshark_gtk=OFF
-      -DENABLE_PORTAUDIO=OFF
-      -DENABLE_LUA=ON
-      -DLUA_INCLUDE_DIR=#{Formula["lua@5.1"].opt_include}/lua-5.1
-      -DLUA_LIBRARY=#{Formula["lua@5.1"].opt_lib}/liblua5.1.dylib
-      -DCARES_INCLUDE_DIR=#{Formula["c-ares"].opt_include}
-      -DGCRYPT_INCLUDE_DIR=#{Formula["libgcrypt"].opt_include}
-      -DGNUTLS_INCLUDE_DIR=#{Formula["gnutls"].opt_include}
-      -DMAXMINDDB_INCLUDE_DIR=#{Formula["libmaxminddb"].opt_include}
-      -DENABLE_SMI=ON
-      -DBUILD_sshdump=ON
-      -DBUILD_ciscodump=ON
-
-      -DENABLE_NGHTTP2=ON
-      -DBUILD_wireshark=OFF
-      -DENABLE_APPLICATION_BUNDLE=OFF
-      -DENABLE_QT5=OFF
-    ]
-
-    system "cmake", *args, "."
-    system "make", "install"
-
-    # Install headers
-    (include/"wireshark").install Dir["*.h"]
-    (include/"wireshark/epan").install Dir["epan/*.h"]
-    (include/"wireshark/epan/crypt").install Dir["epan/crypt/*.h"]
-    (include/"wireshark/epan/dfilter").install Dir["epan/dfilter/*.h"]
-    (include/"wireshark/epan/dissectors").install Dir["epan/dissectors/*.h"]
-    (include/"wireshark/epan/ftypes").install Dir["epan/ftypes/*.h"]
-    (include/"wireshark/epan/wmem").install Dir["epan/wmem/*.h"]
-    (include/"wireshark/wiretap").install Dir["wiretap/*.h"]
-    (include/"wireshark/wsutil").install Dir["wsutil/*.h"]
-  end
-
-  def caveats; <<~EOS
-    This formula only installs the command-line utilities by default.
-
-    Install Wireshark.app with Homebrew Cask:
-      brew cask install wireshark
-
-    If your list of available capture interfaces is empty
-    (default macOS behavior), install ChmodBPF:
-      brew cask install wireshark-chmodbpf
-  EOS
-  end
-
-  test do
-    system bin/"randpkt", "-b", "100", "-c", "2", "capture.pcap"
-    output = shell_output("#{bin}/capinfos -Tmc capture.pcap")
-    assert_equal "File name,Number of packets\ncapture.pcap,2\n", output
-  end
+  zap trash: '~/Library/Saved Application State/org.wireshark.Wireshark.savedState'
 end
