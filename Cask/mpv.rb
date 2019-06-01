d5850ba13e1e13f7aef71a76aa9a6ce506a2f996
Jam <jianliu@newbanker.cn>
2019-06-01 16:17:39 +0800

    syn 同步当前应用版本 @2019060116

--- Cask/mpv.rb
+++ Cask/mpv.rb
@@ -1,64 +1,22 @@
-class Mpv < Formula
-  desc "Media player based on MPlayer and mplayer2"
-  homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz"
-  sha256 "f9f9d461d1990f9728660b4ccb0e8cb5dce29ccaa6af567bec481b79291ca623"
-  revision 3
-  head "https://github.com/mpv-player/mpv.git"
+cask 'mpv' do
+  version '0.29.1'
+  sha256 '45d176bc884dfa84ce5226f53bc0809e2054984837e16a1518061e5493700f21'
 
-  bottle do
-    sha256 "a91d2f0d616a23d37308c5a0c1f4902b07eec44f2eb6619c285044d3e4bb0124" => :mojave
-    sha256 "27b27bc1bfe887f696b5c625dc5ac5dab5806a02cfa6104be1214e1eb6d3ec53" => :high_sierra
-    sha256 "61471c7206414f25b4c23da82b239197000d4d94104fdd51e0893e07d44b8737" => :sierra
-  end
+  # laboratory.stolendata.net/~djinn/mpv_osx was verified as official when first introduced to the cask
+  url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}.tar.gz"
+  appcast 'https://laboratory.stolendata.net/~djinn/mpv_osx/'
+  name 'mpv'
+  homepage 'https://mpv.io/'
 
-  depends_on "docutils" => :build
-  depends_on "pkg-config" => :build
-  depends_on "python" => :build
+  depends_on macos: '>= :yosemite'
 
-  depends_on "ffmpeg"
-  depends_on "jpeg"
-  depends_on "libarchive"
-  depends_on "libass"
-  depends_on "little-cms2"
-  depends_on "lua@5.1"
+  app 'mpv.app'
+  binary "#{appdir}/mpv.app/Contents/MacOS/mpv"
 
-  depends_on "mujs"
-  depends_on "uchardet"
-  depends_on "vapoursynth"
-  depends_on "youtube-dl"
-
-  def install
-    # LANG is unset by default on macOS and causes issues when calling getlocale
-    # or getdefaultlocale in docutils. Force the default c/posix locale since
-    # that's good enough for building the manpage.
-    ENV["LC_ALL"] = "C"
-
-    args = %W[
-      --prefix=#{prefix}
-      --enable-html-build
-      --enable-javascript
-      --enable-libmpv-shared
-      --enable-lua
-      --enable-libarchive
-      --enable-uchardet
-      --confdir=#{etc}/mpv
-      --datadir=#{pkgshare}
-      --mandir=#{man}
-      --docdir=#{doc}
-      --enable-zsh-comp
-      --zshdir=#{zsh_completion}
-    ]
-
-    system "./bootstrap.py"
-    system "python3", "waf", "configure", *args
-    system "python3", "waf", "install"
-
-    system "python3", "TOOLS/osxbundle.py", "build/mpv"
-    prefix.install "build/mpv.app"
-  end
-
-  test do
-    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
-  end
+  zap trash: [
+               '~/.config/mpv',
+               '~/Library/Logs/mpv.log',
+               '~/Library/Preferences/io.mpv.plist',
+               '~/Library/Preferences/mpv.plist',
+             ]
 end
--- Cask/syncthing.rb
+++ Cask/syncthing.rb
@@ -1,70 +1,22 @@
-class Syncthing < Formula
-  desc "Open source continuous file synchronization application"
-  homepage "https://syncthing.net/"
-  url "https://github.com/syncthing/syncthing.git",
-      :tag      => "v1.1.3",
-      :revision => "a04fcfe7494a6e3ede091534597539b77dc05a52"
-  head "https://github.com/syncthing/syncthing.git"
+cask 'syncthing' do
+  version '1.0.0-2'
+  sha256 '71b8796d4e8801acc7e8176c7e587a50a8960adaabac7fa94ad0f4c72b32bad5'
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "acd22bd0d4e06746e160caf08c4f81aa8a3af3df51a0b794f8ebb8a841d4cf44" => :mojave
-    sha256 "d7c4d1e762634eaf2fe1dd6d7550812bc7077fe09dbda305809144ddb443068c" => :high_sierra
-    sha256 "2dfe762c366a512885fe54ff214bf139eeca643a3bbf968deba86a1f12746936" => :sierra
-  end
+  # github.com/syncthing/syncthing-macos was verified as official when first introduced to the cask
+  url "https://github.com/syncthing/syncthing-macos/releases/download/v#{version}/Syncthing-#{version}.dmg"
+  appcast 'https://github.com/syncthing/syncthing-macos/releases.atom'
+  name 'Syncthing'
+  homepage 'https://syncthing.net/'
 
-  depends_on "go" => :build
+  auto_updates true
+  depends_on macos: '>= :yosemite'
 
-  def install
-    ENV["GO111MODULE"] = "on"
-    ENV["GOPATH"] = buildpath
+  app 'Syncthing.app'
 
-    src = buildpath/"src/github.com/syncthing/syncthing"
-    src.install buildpath.children
-    src.cd do
-      system "./build.sh", "noupgrade"
-      bin.install "syncthing"
-      man1.install Dir["man/*.1"]
-      man5.install Dir["man/*.5"]
-      man7.install Dir["man/*.7"]
-      prefix.install_metafiles
-    end
-  end
-
-  plist_options :manual => "syncthing"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/syncthing</string>
-          <string>-no-browser</string>
-          <string>-no-restart</string>
-        </array>
-        <key>KeepAlive</key>
-        <dict>
-          <key>Crashed</key>
-          <true/>
-          <key>SuccessfulExit</key>
-          <false/>
-        </dict>
-        <key>ProcessType</key>
-        <string>Background</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/syncthing.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/syncthing.log</string>
-      </dict>
-    </plist>
-  EOS
-  end
-
-  test do
-    system bin/"syncthing", "-generate", "./"
-  end
+  zap trash: [
+               '~/Library/Application Support/Syncthing-macOS',
+               '~/Library/Caches/com.github.xor-gate.syncthing-macosx',
+               '~/Library/Cookies/com.github.xor-gate.syncthing-macosx.binarycookies',
+               '~/Library/Preferences/com.github.xor-gate.syncthing-macosx.plist',
+             ]
 end
