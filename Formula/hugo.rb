53d1a624789ca733f2f1bb70f797f4c5d38c0f33
Jam <liujian@mail.com>
2021-02-28 12:58:52 +0800

    upd 更新hugo至最新版本

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,16 +1,18 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.66.0.tar.gz"
-  sha256 "58716ebccbf34e4a4e04663c68c5f94d90e920314b44f4e8235ff9bd57604d4f"
+  version "v0.80.0"
+  url "https://github.com/gohugoio/hugo/archive/#{version}.tar.gz"
+  # url "http://localhost:8000/#{version}.tar.gz"
+  sha256 "4ddcd6ebea21e5fd4067db4a481ab7810e34496d5991a3520169c8f5ee1d38bb"
   head "https://github.com/gohugoio/hugo.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "d8eb42640a7344366faad179974974127d1d3ddd95a9603b71d80308f4b1cacc" => :catalina
-    sha256 "d4fbd66ecdbd4ccbf7d5e7fe00f9b38716862be8fe067266b3d3b60a91dc18e8" => :mojave
-    sha256 "d65886d28f029eda5230c296a5de24e16ef636ecce7d938997a7a954d10f33c6" => :high_sierra
-  end
+  # bottle do
+  #   cellar :any_skip_relocation
+  #   sha256 "d8eb42640a7344366faad179974974127d1d3ddd95a9603b71d80308f4b1cacc" => :catalina
+  #   sha256 "d4fbd66ecdbd4ccbf7d5e7fe00f9b38716862be8fe067266b3d3b60a91dc18e8" => :mojave
+  #   sha256 "d65886d28f029eda5230c296a5de24e16ef636ecce7d938997a7a954d10f33c6" => :high_sierra
+  # end
 
   depends_on "go" => :build
 
@@ -19,9 +21,10 @@ class Hugo < Formula
     (buildpath/"src/github.com/gohugoio/hugo").install buildpath.children
 
     cd "src/github.com/gohugoio/hugo" do
+      # ohai "Test output : $PATH"
+      system "go", "env", "-w", "GOPROXY=https://goproxy.cn,direct"
       system "go", "build", "-o", bin/"hugo", "-tags", "extended", "main.go"
 
-      # Build bash completion
       system bin/"hugo", "gen", "autocomplete", "--completionfile=hugo.sh"
       bash_completion.install "hugo.sh"
 
